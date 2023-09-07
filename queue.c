9de5882868bd46c2de8c2510a34a68af18d372a6
Subinay <subinayp108@gmail.com>
2023-09-07 20:58:44 +0530

    queue added

--- queue.c
+++ queue.c
@@ -4,79 +4,73 @@
 
 struct Queue{
     int first, last, size, arr[100];
-};
+} Queue_default = {-1, -1, 100, {}};
 
 typedef struct Queue Queue;
 
-void initialize(Queue);
-void enqueue(Queue, int item);
-int dequeue(Queue);
-void display(Queue);
+void enqueue(Queue*, int item);
+int dequeue(Queue*);
+void display(Queue*);
 
 void main(){
-    Queue q1;
+    Queue q1 = Queue_default;
     int choice;
     while(1){
         printf("1. Enqueue\n2. Dequeue\n3. Display\n4. Exit\n");
-        printf("Enter your chioce: ");
+        printf("Enter your choice: ");
         scanf("%d", &choice);
         int item;
         switch(choice){
             case 1:
                 printf("Enter an item to enqueue: ");
                 scanf("%d", &item);
-                enqueue(q1, item);
+                enqueue(&q1, item);
                 break;
             case 2:
-                item = dequeue(q1);
-                printf("Deleted item: %d", item);
+                item = dequeue(&q1);
+                printf("Deleted item: %d\n", item);
                 break;
             case 3:
-                display(q1);
+                display(&q1);
+                printf("\n");
                 break;
             case 4:
                 printf("Exit");
                 exit(0);
                 break;
             default:
-                printf("Invalid input");
+                printf("Invalid input\n");
         }
     }
     getchar();
 }
 
-void enqueue(Queue q, int item){
-    if(q.last == q.size - 1){
-        printf("Overflow");
+void enqueue(Queue *q, int item){
+    if(q->last == q->size - 1){
+        printf("Overflow\n");
         return;
     }
-    if(q.first == -1 && q.last == -1){
-        q.first++;
+    if(q->first == -1 && q->last == -1){
+        q->first++;
     }
-    q.last++;
-    q.arr[q.last] = item;
+    q->last++;
+    q->arr[q->last] = item;
 }
 
-int dequeue(Queue q){
-    if((q.first == -1 && q.last == -1) || (q.first > q.last)){
-        printf("Underflow");
+int dequeue(Queue *q){
+    if((q->first == -1 && q->last == -1) || (q->first > q->last)){
+        printf("Underflow\n");
         return 0;
     }
-    int item = q.first;
-    q.first++;
+    int item = q->arr[q->first];
+    q->first++;
     return item;
 }
 
-void display(Queue q){
+void display(Queue *q){
     int i;
     printf("Items in Queue are: ");
-    for(i = 0; i < q.size; i++){
-        printf("%d ", q.arr[i]);
+    for(i = q->first; i <= q->last; i++){
+        printf("%d ", q->arr[i]);
     }
-}
-
-void initialize(Queue q){
-    q.first = -1;
-    q.last = -1;
-    q.size = 100;
 }
