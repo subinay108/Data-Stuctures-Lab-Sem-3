982c825745c83d2cb449e4dad0a461f8a0ac0669
Subinay <subinayp108@gmail.com>
2023-09-27 18:00:51 +0530

    Singly Linked List

---
+++ singlyLinkedList.c
@@ -0,0 +1,61 @@
+// Implementation of Singly Linked List in C
+#include<stdio.h>
+
+typedef struct Node{
+    int data;
+    struct Node *next;
+} Node;
+ 
+Node* createList();
+
+Node* createNode(int);
+
+void insertAtFirst(int, Node*);
+void insertAtLast(Node*);
+void insertAfter(Node*);
+void insertBefore(Node*);
+void insertAtPos(Node*);
+
+void display(Node*);
+
+void main(){
+	list = createList();
+	int i;
+	for(i = 0; i < 10; i++){
+		insertAtFirst(&list,i);
+	}
+	display(list);
+}
+
+void display(Node *head){
+	Node* temp = head;
+	while(temp != NULL){
+		printf("%d ", temp->data);
+		temp = temp->next;
+	}
+	printf("%d\n", temp->data);
+}
+
+void insertAtFirst(Node* list, int value){
+	Node* head = *list;
+	// create a node
+	Node* n = createNode(value);
+	
+	// set new node next to head
+	n->next = head;
+	
+	// set head to address of new node
+	*head = n;
+}
+
+Node* createNode(int value){
+	Node* n = (Node *) malloc(sizeof(Node));
+	n->data = value;
+	n->next = NULL;
+	return n;
+}
+
+Node* createList(){
+	Node *n = NULL;
+	return n;
+}
