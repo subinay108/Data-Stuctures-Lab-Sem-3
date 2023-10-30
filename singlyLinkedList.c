075879b9e4379629812793c33b2245641ac9bea3
subinay108 <subinayp108@gmail.com>
2023-10-30 21:50:39 +0530

    singly linked list updated

--- singlyLinkedList.c
+++ singlyLinkedList.c
@@ -1,6 +1,7 @@
 // Implementation of Singly Linked List in C
 #include<stdio.h>
 #include<stdlib.h>
+#include<conio.h>
 
 typedef struct Node{
     int data;
@@ -26,16 +27,78 @@ void deleteAtPos(Node**, int);
 // Display
 void display(Node*);
 
+// Get length of a list
+int getLength(Node**);
+
 int main(){
 	Node* list = createList();
-	int i;
-	for(i = 1; i < 11; i++){
-		insertAtLast(&list, i);
+	int choice, element, pos;
+	while(1){
+		system("cls");
+		printf("Singly Linked List\n1.Insert at First\n2.Insert at Last\n\
+3.Insert at Position\n4.Insert After\n5.Insert Before\n6.Delete at First\n7.Delete at Last\n\
+8.Delete at Position\n9.Display\n0.Exit\nEnter your choice:");
+		scanf("%d", &choice);
+		switch(choice){
+			case 1:
+				printf("Enter element: ");
+				scanf("%d", &element);
+				insertAtFirst(&list, element);
+				printf("%d added at First\nEnter any key to proceed...", element);
+				getch();
+				break;
+			case 2:
+				printf("Enter element: ");
+				scanf("%d", &element);
+				insertAtLast(&list, element);
+				printf("%d added at Last\nEnter any key to proceed...", element, pos);
+				getch();
+				break;
+			case 3:
+				printf("Enter element: ");
+				scanf("%d", &element);
+				printf("Enter position: ");
+				scanf("%d", &pos);
+				insertAtPos(&list, pos, element);
+				printf("%d added at position %d\nEnter any key to proceed...", element, pos);
+				getch();
+				break;
+			case 4:
+				break;
+			case 5:
+				break;
+			case 6:
+				deleteAtFirst(&list);
+				printf("\nEnter any key to proceed...");
+				getch();
+				break;
+			case 7:
+				deleteAtLast(&list);
+				printf("\nEnter any key to proceed...");
+				getch();
+				break;
+			case 8:
+				printf("Enter position: ");
+				scanf("%d", &pos);
+				deleteAtPos(&list, pos);
+				printf("\nEnter any key to proceed...");
+				getch();
+				break;
+			case 9:
+				printf("The list is: ");
+				display(list);
+				printf("\nEnter any key to proceed...");
+				getch();
+				break;
+			case 0:
+				exit(0);
+				break;
+			default:
+				printf("Invalid input");
+				printf("\nEnter any key to proceed...");
+				getch();
+		}
 	}
-	deleteAtFirst(&list);
-	deleteAtLast(&list);
-	deleteAtPos(&list, 1);
-	display(list);
 	return 0;
 }
 
@@ -118,6 +181,21 @@ void display(Node* head){
 	}
 }
 
+void insertAtPos(Node** list, int pos, int value){
+	Node* head = *list;
+	// if position is 0
+	if(pos == 0){
+		insertAtFirst(list, pos, value);
+		return;
+	}
+	// position other than 0
+	Node* h = *list;
+	
+	while(--pos){
+		h = h->next;	
+	}
+	
+}
 void insertAtLast(Node** list, int value){
 	Node* head = *list;
 	// create a node
